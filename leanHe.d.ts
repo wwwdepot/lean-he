515aa443b1d31aa82568ebe5e5e07dfdf2c089f9
wwwdepot <53346173+wwwdepot@users.noreply.github.com>
2022-04-18 15:19:02 -0400

    Create leanHe.d.ts

---
+++ leanHe.d.ts
@@ -0,0 +1,23 @@
+interface Options {
+  /** default `false` */
+  decimal?: boolean;
+  /** default `false` */
+  useNamedReferences?: boolean;
+  /** default `false` */
+  encodeEverything?: boolean;
+  /** default `false` */
+  strict?: boolean;
+  /** default `false` */
+  allowUnsafeSymbols?: boolean;
+}
+
+interface DecodeOptions {
+  /** default `false` */
+  isAttributeValue?: boolean;
+  /** default `false` */
+  strict?: boolean;
+}
+export function escape(text: string, options?: Options): string;
+export function encode(text: string, options?: Options): string;
+export function decode(html: string, options?: DecodeOptions): string;
+export function unescape(html: string, options?: DecodeOptions): string;
